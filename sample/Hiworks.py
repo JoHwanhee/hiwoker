bef60540e6d194b5ab4d703b79d014c992cd63f9
hhjo <kikiki0611@naver.com>
2019-09-16 02:41:36 +0900

    Add samples

---
+++ sample/Hiworks.py
@@ -0,0 +1,69 @@
+from abc import *
+from flask import Flask, Response, request, url_for, redirect
+import json
+
+class hiworksBotServer(metaclass=ABCMeta):
+    def __init__(self, key):
+        self.guides = []
+        self.wrapper = None
+        self.key = key
+        self.wrapper = FlaskAppWrapper('wrap')
+
+    def setGuides(self, guides):
+        self.guides = guides
+    
+    def addHandler(self, name, handler):
+        self.wrapper.add_endpoint(endpoint='/%s' % name, endpoint_name=name, handler=handler, methods=['POST'])
+
+    def run(self, host = '127.0.0.1', port = 5000):
+        self.wrapper.add_endpoint(endpoint='/', endpoint_name='init', handler=self.init, methods=['GET'])
+        self.wrapper.add_endpoint(endpoint='/prev', endpoint_name='prev', handler=self.prev, methods=['POST'])
+        self.wrapper.run(host, port)
+
+    def init(self):
+        return self.wrapper.app.response_class(
+            response=json.dumps(self.guides, default=lambda o: o.__dict__, sort_keys=True, indent=4),
+            status=200,
+            mimetype='application/json; charset=utf-8"'
+        )
+
+    def prev(self):
+        return redirect(url_for(request.json['key']), code=307)
+
+class FlaskAppWrapper(object):
+    app = None
+
+    def __init__(self, name):
+        self.app = Flask(name)
+
+    def run(self, host, port):
+        self.app.run(host, port)
+
+    def add_endpoint(self, endpoint=None, endpoint_name=None, handler=None, methods =None):
+        self.app.add_url_rule(endpoint, endpoint_name, handler,methods=  methods)
+
+
+class GuideToken:
+    def __init__(self, key, displayName):
+        self.key = key
+        self.displayName = displayName
+        self.userCommandTokens = []
+
+    @classmethod
+    def from_json(cls, data):
+        userCommandTokens = list(map(UserCommandToken.from_json, data["userCommandTokens"]))
+        return cls(userCommandTokens)
+
+class UserCommandToken:
+    def __init__(self, key, displayName):
+        self.key = key
+        self.displayName = displayName
+        self.value = ''
+        self.containsVariable = ''
+        self.contentOnly = False
+        self.canValues = []
+        self.allValues = []
+
+    @classmethod
+    def from_json(cls, data):
+        return cls(**data)
--- sample/greeting.py
+++ sample/greeting.py
@@ -1,2 +1,42 @@
-def hello():
-    print('Hello world.')
+from Hiworks import *
+
+def run():
+    guides = []
+    guide = GuideToken('book', '/예약')
+    commandToken = UserCommandToken('room', "%회의실명%")
+    commandToken.allValues = ['S1','S2','S3','S4','S5','M','대 회의실']
+    commandToken.canValues = ['S1','S2','S3']
+
+    s1Token = UserCommandToken('time_1', "%예약할 시간%")
+    s1Token.containsKey = 'room'
+    s1Token.containsValue = 'S1'
+    s1Token.allValues = ["09:00~09:30", "10:00~10:30","10:30~11:00"]
+    s1Token.canValues = ["09:00~09:30"]
+
+    s2Token = UserCommandToken('time_2', "%예약할 시간%")
+    s2Token.containsKey = 'room'
+    s2Token.containsValue = 'S2'
+    s2Token.allValues = ["09:00~09:30", "10:00~10:30","10:30~11:00"]
+    s2Token.canValues = ["09:00~09:30", "10:00~10:30","10:30~11:00"]
+
+    s3Token = UserCommandToken('time_3', "%예약할 시간%")
+    s3Token.containsKey = 'room'
+    s3Token.containsValue = 'S3'
+    s3Token.allValues = ["09:00~09:30", "10:00~10:30","10:30~11:00"]
+    s3Token.canValues = ["09:00~09:30", "10:30~11:00"]
+
+
+    guide.userCommandTokens = [commandToken, s1Token, s2Token, s3Token]
+    guides.append(guide)
+
+    server = hiworksBotServer('')
+    server.setGuides(guides)
+
+    server.addHandler('book', book)
+    server.run()
+
+def book():
+    print(request.json['key'])
+    return 'ok'
+
+run()
