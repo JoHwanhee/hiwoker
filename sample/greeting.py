9483539d7d56904e3e240ab1f66b0246e0c54ecd
hhjo <kikiki0611@naver.com>
2019-09-23 01:58:14 +0900

    add google calendar

--- sample/greeting.py
+++ sample/greeting.py
@@ -1,89 +1,127 @@
+from __future__ import print_function
 from Hiworks import *
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
-def makeBugGuide():
-    guide = GuideToken('bug', '/버그')
-    commandToken = UserCommandToken('bug', '%버그 발생 시점 및 현상%')
-    commandToken.contentOnly = True
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-    guide.userCommandTokens = [commandToken]
-    return guide
+def addGoogleEvent(startTime, endTime, title):
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
-def makeBookGuide():
+    service = build('calendar', 'v3', credentials=creds)
+    now = datetime.datetime.utcnow().isoformat() + 'Z'
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+
+    today = datetime.date.today().strftime('%Y-%m-%d')
+    event = {
+    'summary': title,
+    'location': 'Test',
+    'description': title,
+    'start': {
+        'dateTime': '%sT%s:00+09:00' % (today, startTime),
+        'timeZone': 'Asia/Seoul',
+    },
+    'end': {
+        'dateTime': '%sT%s:00+09:00' % (today, endTime),
+        'timeZone': 'Asia/Seoul',
+    },
+    'recurrence': [
+        'RRULE:FREQ=DAILY;COUNT=1'
+    ],
+    'attendees': [
+    ],
+    'reminders': {
+        # 'useDefault': False,
+        # 'overrides': [
+        # {'method': 'email', 'minutes': 24 * 60},
+        # {'method': 'popup', 'minutes': 10},
+        # ],
+    },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print ('Event created: %s' % (event.get('htmlLink')))
+
+def run():
+    guides = []
     guide = GuideToken('book', '/예약')
     commandToken = UserCommandToken('room', "%회의실명%")
     commandToken.allValues = ['S1','S2','S3','S4','S5','M','대 회의실']
     commandToken.canValues = ['S1','S2','S3']
 
-    s1Token = UserCommandToken('time_1', "%예약할 시간%")
-    s1Token.containsKey = 'room'
-    s1Token.containsValue = 'S1'
-    s1Token.allValues = ["09:00~09:30", "10:00~10:30","10:30~11:00"]
-    s1Token.canValues = ["09:00~09:30"]
-
-    s2Token = UserCommandToken('time_2', "%예약할 시간%")
-    s2Token.containsKey = 'room'
-    s2Token.containsValue = 'S2'
-    s2Token.allValues = ["09:00~09:30", "10:00~10:30","10:30~11:00"]
-    s2Token.canValues = ["09:00~09:30", "10:00~10:30","10:30~11:00"]
-
-    s3Token = UserCommandToken('time_3', "%예약할 시간%")
-    s3Token.containsKey = 'room'
-    s3Token.containsValue = 'S3'
-    s3Token.allValues = ["09:00~09:30", "10:00~10:30","10:30~11:00"]
-    s3Token.canValues = ["09:00~09:30", "10:30~11:00"]
-
-    guide.userCommandTokens = [commandToken, s1Token, s2Token, s3Token]
-    return guide
-
-def makeVoteGuide():
-    guide = GuideToken('vote', '/투표')
-    commandToken = UserCommandToken('room', "%[질문]%")
-    commandToken.contentOnly = True
-
-    s1Token = UserCommandToken('time_1', "%후보%")
-    s1Token.contentOnly = True
+    s1Token = UserCommandToken('time', "%예약할 시간%")
+    #s1Token.allValues = ["09:00~09:30", "10:00~10:30","10:30~11:00"]
+    s1Token.canValues = ["09:00-09:30", "09:30-10:00","10:00-10:30"]   
 
     guide.userCommandTokens = [commandToken, s1Token]
-    return guide
 
-def makeNoticeGuide():
-    guide = GuideToken('notice', '/공지')
-    commandToken = UserCommandToken('notice', "%공지할내용%")
-    commandToken.contentOnly = True
-    
-    guide.userCommandTokens = [commandToken]
-    return guide
+    guide2 = GuideToken('bug', '/버그')
+    commandToken2 = UserCommandToken('file', "%버그 발생 시점 및 현상%")
+    commandToken2.contentOnly = True
 
-def makeGuides():
-    guides = []
-    guides.append(makeBugGuide())
-    guides.append(makeBookGuide())
-    guides.append(makeVoteGuide())
-    guides.append(makeBookGuide())
+    guide2.userCommandTokens = [commandToken2]
 
-    return guides
+    guides.append(guide)
+    guides.append(guide2)
 
-def run():
     server = hiworksBotServer('')
-    server.setGuides(makeGuides())
+    server.setGuides(guides)
 
     server.addHandler('book', book)
     server.addHandler('bug', bug)
-    server.addHandler('vote', vote)
-    server.addHandler('notice', notice)
     server.run()
-
+    
 def book():
-    print(request.json['key'])
-    return '예약 되었습니다!!!'
+    print(request.json)
+    userCommandTokens = request.json['userCommandTokens']
+    room = ''
+    time = ''
+    for token in userCommandTokens:
+        if token['key'] == 'room':
+            room = token['value']
+        elif token['key'] == 'time':
+            time = token['value']
+    
+    message = time.split('(')[0]
+    times = message.split('-')
+    startTime = times[0]
+    endTime = times[1]
+    addGoogleEvent(startTime, endTime, room)
+
+    return '#BOT;clock;ok;예약이 완료 되었습니다.'
+
 def bug():
-    print(request.json['key'])
-    return '버그 접수 되었습니다.'
-def vote():
-    print(request.json['key'])
-    return '투표 기능은 개발즁입니다..'
-def notice():
-    print(request.json['key'])
-    return '공지 기능은개발중입니다..'
+    print(request.json)
+    
+
+
+    return '#BOT;bug;ok;버그 접수가 완료 되었습니다.'
+
 
 run()
