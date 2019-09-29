5a37a6ae8f947deb978777f3776ac7204a2e6e2b
Jayden <kikiki0611@naver.com>
2019-09-29 23:59:42 +0900

    modify sameple sources for presentation

--- sample/greeting.py
+++ sample/greeting.py
@@ -68,60 +68,104 @@ def addGoogleEvent(startTime, endTime, title):
     event = service.events().insert(calendarId='primary', body=event).execute()
     print ('Event created: %s' % (event.get('htmlLink')))
 
-def run():
-    guides = []
+def makeBook():
     guide = GuideToken('book', '/예약')
     commandToken = UserCommandToken('room', "%회의실명%")
     commandToken.allValues = ['S1','S2','S3','S4','S5','M','대 회의실']
     commandToken.canValues = ['S1','S2','S3']
 
     s1Token = UserCommandToken('time', "%예약할 시간%")
-    #s1Token.allValues = ["09:00~09:30", "10:00~10:30","10:30~11:00"]
     s1Token.canValues = ["09:00-09:30", "09:30-10:00","10:00-10:30"]   
 
     guide.userCommandTokens = [commandToken, s1Token]
+    return guide;
 
-    guide2 = GuideToken('bug', '/버그')
-    commandToken2 = UserCommandToken('file', "%버그 발생 시점 및 현상%")
+def makeVote():
+    guide = GuideToken('voat', '/투표')
+    commandToken = UserCommandToken('voat1', "%질문%")
+    commandToken.contentOnly = True
+
+    commandToken2 = UserCommandToken('voat2', "%후보1%")
     commandToken2.contentOnly = True
 
-    guide2.userCommandTokens = [commandToken2]
+    commandToken3 = UserCommandToken('voat3', "%후보2%")
+    commandToken3.contentOnly = True
 
-    guides.append(guide)
-    guides.append(guide2)
+    guide.userCommandTokens = [commandToken, commandToken2, commandToken3]
+
+    return guide
+
+def makeNotice():
+    guide = GuideToken('notice', '/공지')
+    commandToken = UserCommandToken('notice', "%공지할 내용%")
+    commandToken.contentOnly = True
+
+    guide.userCommandTokens = [commandToken]
+
+    return guide
+
+def makeBug():
+    guide = GuideToken('bug', '/버그')
+    commandToken = UserCommandToken('file', "%버그 발생 시점 및 현상%")
+    commandToken.contentOnly = True
+    guide.userCommandTokens = [commandToken]
+
+    return guide
+
+def run():
+    guides = []
+    
+
+    guides.append(makeBook())
+    guides.append(makeBug())
+    guides.append(makeVote())
+    guides.append(makeNotice())
+
 
     server = hiworksBotServer('dd7942ff-bd6a-4cdc-b367-7261a0f78e17')
     server.setGuides(guides)
 
     server.addHandler('book', book)
     server.addHandler('bug', bug)
+    server.addHandler('voat', voat)
+    server.addHandler('notice', notice)
     server.run()
     
 def book():
     print(request.json)
-    userCommandTokens = request.json['userCommandTokens']
-    room = ''
-    time = ''
-    for token in userCommandTokens:
-        if token['key'] == 'room':
-            room = token['value']
-        elif token['key'] == 'time':
-            time = token['value']
-    
-    message = time.split('(')[0]
-    times = message.split('-')
-    startTime = times[0]
-    endTime = times[1]
-    addGoogleEvent(startTime, endTime, room)
+    try:
+        userCommandTokens = request.json['userCommandTokens']
+        room = ''
+        time = ''
+        for token in userCommandTokens:
+            if token['key'] == 'room':
+                room = token['value']
+            elif token['key'] == 'time':
+                time = token['value']
+        message = time.split('(')[0]
+        times = message.split('-')
+        startTime = times[0]
+        endTime = times[1]
+        addGoogleEvent(startTime, endTime, room)
 
-    return '#BOT;clock;ok;예약이 완료 되었습니다.'
+        return '#BOT;clock;ok;예약이 완료 되었습니다.;미팅룸 ' + room + ' ' + startTime + '-' + endTime
+    except:
+        return '#BOT;exclamation-circle;fail;정확한 회의실 장소와 시각을 입력해주세요.'
+
 
 def bug():
     print(request.json)
-    
 
+    return '#BOT;bug;ok;버그 리포팅이 완료 되었습니다.'
 
-    return '#BOT;bug;ok;버그 접수가 완료 되었습니다.'
+def voat():
+    print(request.json)
 
+    return '#BOT;poll-h;ok;투표 등록 되었습니다.'
+
+def notice():
+    print(request.json)
+
+    return '#BOT;exclamation-circle;ok;공지 등록 되었습니다.'
 
 run()
